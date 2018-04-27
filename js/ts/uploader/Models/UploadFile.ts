858fc15aaa4819830e6a22f09f709d6eb5e5cda0
xieguigang <xie.guigang@gcmodeller.org>
2018-04-27 13:00:49 +0800

    config project

--- js/ts/uploader/Models/UploadFile.ts
+++ js/ts/uploader/Models/UploadFile.ts
@@ -69,17 +69,3 @@ class UploadFile {
         }
     }
 }
-
-/**
- * 文件对象类型枚举
-*/
-enum fileObjectTypes {
-    /**
-     * 这是一个文件
-    */
-    file,
-    /**
-     * 这是一个文件夹
-    */
-    directory
-};
---
+++ js/ts/uploader/Models/fileObjectTypes.ts
@@ -0,0 +1,14 @@
+
+/**
+ * 文件对象类型枚举
+*/
+enum fileObjectTypes {
+    /**
+     * 这是一个文件
+    */
+    file,
+    /**
+     * 这是一个文件夹
+    */
+    directory
+};
