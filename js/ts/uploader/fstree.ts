e1bdd659523f82d6e8b6d1f035ad6f000aa8d5d8
谢桂纲 <xie.guigang@gcmodeller.org>
2018-04-27 23:06:06 +0800

    code rollback

--- js/ts/uploader/fstree.ts
+++ js/ts/uploader/fstree.ts
@@ -6,45 +6,46 @@
      * 根据文件路径构建出文件系统树
     */
     constructor(files: any[]) {
-        var root: UploadFile = new UploadFile(null, "/", -1, null);
-        var childs: UploadFile[] = [];
-
-        childs["/"] = root;
-
-        /**
-     * 返回路径所指示的最后一个文件夹，如果不存在，则会进行创建
-    */
-        function travelToParent(path: string[]): UploadFile {
-            var dir: string = path.pop();
-            var name: string = path[path.length - 1];
-
-            dir = path.join("/");
-
-            console.log(path.join("/") + " => " + path[path.length - 2] + " => " + dir);
-
-            if (dir in childs) {
-                return childs[dir];
-            } else {
-                var parent: UploadFile = travelToParent(path);
-                var folder: UploadFile = new UploadFile(null, name, 0, parent);
-
-                childs[dir] = folder;
-
-                return folder;
-            }
-        }
+        this.root = new UploadFile(null, "/", -1, null);
 
         files.forEach(file => {
             var path: string[] = file.filepath.split("/");
             var name: string = file.name;
             var size: number = file.size;
-            var folder: UploadFile = travelToParent(path);
+            var folder: UploadFile = this.travelToParent(path);
             var obj: UploadFile = new UploadFile(file, name, size, folder);
 
             folder.addChild(obj);
         });
+    }
 
-        console.log(childs);
+    /**
+     * 返回路径所指示的最后一个文件夹，如果不存在，则会进行创建
+    */
+    private travelToParent(path: string[]): UploadFile {
+        var parent: UploadFile = this.root;
+
+        console.log(path.join("/") + " => " + path[path.length - 2]);
+
+        for (var i = 0; i < path.length - 2; ++i) {
+            var name: string = path[i];
+            var folder: UploadFile = null;
+
+            console.log(name);
+
+            if (parent.hasChild(name)) {
+                folder = parent.childs[name];
+                console.log("get");
+            } else {
+                folder = new UploadFile(null, name, 0, parent);
+                parent.addChild(folder);
+                console.log("build");
+            }
+
+            parent = folder;
+        }
+
+        return parent;
     }
 }
 
